834163337c6a350d15ac1555d5c1ec237bfcf029
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2021-05-06 13:07:41 -0700

    Update main.go
    * Refactored code so that the parameters can be enter in any case
    * Removed debug statements
    * Cleanup ParseArgs function
    * Refactored code so that building factor and prime map are handling in separate functions.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/sflewis2970/factorsandprimes/algebra"
 	"github.com/sflewis2970/factorsandprimes/mathutils"
@@ -12,13 +13,12 @@ import (
 
 const (
 	// FACTOR ...Find factors of values
-	FACTOR = "-factor"
+	FACTOR = "-FACTOR"
 	// PRIME ...Determine if numbers are prime numbers
-	PRIME = "-prime"
+	PRIME = "-PRIME"
 	// GENERATE ...Generate {N} numbers
-	GENERATE = "-generate"
 	// MAXVALUE ...Max value of generated number
-	MAXVALUE = "-maxvalue"
+	GENERATE = "-GENERATE"
 )
 
 type applOptions struct {
@@ -29,61 +29,62 @@ type applOptions struct {
 }
 
 func parseArgs(applArgsList []string) applOptions {
-	fmt.Println("Entering parseArgs...")
-
 	// Declare applOptionData object
 	var applOptionsData applOptions
 
 	// Parse application arguments
 	applArgsListSize := len(applArgsList)
+
 	for idx := 0; idx < applArgsListSize; idx++ {
-		switch applArgsList[idx] {
+		switch strings.ToUpper(applArgsList[idx]) {
 		case FACTOR:
 			applOptionsData.factor = true
 		case PRIME:
 			applOptionsData.prime = true
 		case GENERATE:
-			// Capture the numbers to generate value
-			nbrsToGenerateItem := idx + 1
-			if nbrsToGenerateItem < applArgsListSize {
-				nbrsToGenerate, err := strconv.Atoi(applArgsList[nbrsToGenerateItem])
-				errorMsg := fmt.Sprintf("Error: %v\n", err)
-				if err != nil {
-					log.Fatal(errorMsg)
+			// Calculate number of parameters remaining
+			nbrsOfParamsRemaining := (applArgsListSize - 1) - idx
+
+			// Parse the remainging parameters
+			if nbrsOfParamsRemaining > 0 {
+				// advance the loop pointer
+				idx++
+
+				if idx < applArgsListSize {
+					nbrsToGenerate, err := strconv.Atoi(applArgsList[idx])
+					if err != nil {
+						errorMsg := fmt.Sprintf("Error: %v\n", err)
+						log.Fatal(errorMsg)
+					}
+
+					applOptionsData.nbrsToGenerate = nbrsToGenerate
+				} else {
+					log.Fatal("Error parsing nbrsToGenerate")
 				}
 
-				applOptionsData.nbrsToGenerate = nbrsToGenerate
-			} else {
-				log.Fatal("Error: invalid parameter given.")
-			}
+				nbrsOfParamsRemaining--
+				if nbrsOfParamsRemaining > 0 {
+					// advance the loop pointer
+					idx++
 
-			// Capture the numbers to generate value
-			maxValueItem := idx + 2
-			if maxValueItem < applArgsListSize {
-				maxValue, err := strconv.Atoi(applArgsList[maxValueItem])
-				errorMsg := fmt.Sprintf("Error: %v\n", err)
-				if err != nil {
-					log.Fatal(errorMsg)
+					if idx < applArgsListSize {
+						maxValue, err := strconv.Atoi(applArgsList[idx])
+						if err != nil {
+							errorMsg := fmt.Sprintf("Error: %v\n", err)
+							log.Fatal(errorMsg)
+						}
+
+						applOptionsData.maxValue = maxValue
+					} else {
+						log.Fatal("Error parsing maxValue")
+					}
+				} else {
+					log.Fatal("Error maxValue not found")
 				}
-
-				applOptionsData.maxValue = maxValue
 			} else {
-				log.Fatal("Error: invalid parameter given.")
+				log.Fatal("Error: missing generate parameter values")
 			}
-		case MAXVALUE:
-			nextItem := idx + 1
-			if nextItem < applArgsListSize {
-				maxValue, err := strconv.Atoi(applArgsList[nextItem])
-				errorMsg := fmt.Sprintf("Error: %v\n", err)
-				if err != nil {
-					log.Fatal(errorMsg)
-				}
 
-				applOptionsData.maxValue = maxValue
-				idx++ // Skip the next item
-			} else {
-				log.Fatal("Error: invalid parameter given.")
-			}
 		default:
 			errorMsg := fmt.Sprintf("Error: Option not supported: %s.\n", applArgsList[idx])
 			log.Fatal(errorMsg)
@@ -95,31 +96,64 @@ func parseArgs(applArgsList []string) applOptions {
 		log.Fatal("Usage error: either -factor or -prime flag is required")
 	}
 
-	fmt.Println("Exiting parseArgs...")
-
 	return applOptionsData
+}
+
+func BuildFactorsMap(nbrsList []int) {
+	// Add entries into the map
+	factorsMap, err := algebra.BuildFactorsMap(nbrsList)
+
+	if err != nil {
+		errorMsg := fmt.Sprintf("Error processing Factors, err - %v\n", err)
+		log.Fatal(errorMsg)
+	}
+
+	for key, factorsList := range factorsMap {
+		fmt.Printf("Number: %v, factors: %v\n", key, factorsList)
+	}
+}
+
+func BuildPrimesMap(nbrsList []int) {
+	fmt.Println("Building prime numbers")
+
+	// Add entries into the map
+	primesMap, err := algebra.BuildPrimesMap(nbrsList)
+
+	if err != nil {
+		errorMsg := fmt.Sprintf("Error processing Primes: err - %v\n", err)
+		log.Fatal(errorMsg)
+	}
+
+	if len(primesMap) != 0 {
+		fmt.Println("The following are prime numbers.")
+
+		for key := range primesMap {
+			fmt.Printf("Prime number: %v.\n", key)
+		}
+	} else {
+		fmt.Printf("No prime numbers found.\n")
+	}
 }
 
 func main() {
 	// Starting string
-	fmt.Println("Starting algebra application")
+	fmt.Println("Starting application")
 
-	// Create a slice that contains all parameters with the exception of application name
+	// Strip off the application and store the parameters in the new slice
 	applArgs := os.Args[1:]
 
 	// Declare applOptionsData struct
 	var applOptionsData applOptions
 
-	if len(applArgs) == 0 {
+	if len(applArgs) > 0 {
+		// parse args from command-line
+		applOptionsData = parseArgs(applArgs)
+	} else {
 		// Display usage message
 		fmt.Println("Usage Error")
 
 		// Exit the application
 		log.Fatal("Usage: './main [<-factor>] [<-prime>] [<-generate> [{nbrOfGeneratedValues}] [{maxValue}]]'")
-	} else {
-		// parse args from command-line
-		applOptionsData = parseArgs(applArgs)
-		fmt.Println("Application options data: ", applOptionsData)
 	}
 
 	// Generate numbers requested by user input
@@ -132,7 +166,7 @@ func main() {
 			log.Fatal(errorMsg)
 		}
 
-		// Updatge the numbers list
+		// Update the numbers list
 		nbrsList = newList
 	} else {
 		// Check to see if the nbrsList is empty,
@@ -150,44 +184,14 @@ func main() {
 
 	// Build factors map
 	if applOptionsData.factor {
-		// Add entries into the map
-		factorsMap, err := algebra.BuildFactorsMap(nbrsList)
-
-		if err != nil {
-			errorMsg := fmt.Sprintf("Error processing Factors, err - %v\n", err)
-			log.Fatal(errorMsg)
-		}
-
-		fmt.Println("The following are the factors for the numbers provided.")
-
-		for key, factorsList := range factorsMap {
-			fmt.Printf("Number: %v, factors: %v\n", key, factorsList)
-		}
+		BuildFactorsMap(nbrsList)
 	}
 
 	// Build primes map
 	if applOptionsData.prime {
-		fmt.Println("Building prime numbers")
-
-		// Add entries into the map
-		primesMap, err := algebra.BuildPrimesMap(nbrsList)
-
-		if err != nil {
-			errorMsg := fmt.Sprintf("Error processing Primes: err - %v\n", err)
-			log.Fatal(errorMsg)
-		}
-
-		if len(primesMap) != 0 {
-			fmt.Println("The following are prime numbers.")
-
-			for key := range primesMap {
-				fmt.Printf("Prime number: %v.\n", key)
-			}
-		} else {
-			fmt.Printf("No prime numbers found.\n")
-		}
+		BuildPrimesMap(nbrsList)
 	}
 
 	// Ending string
-	fmt.Println("Exiting Algebra application")
+	fmt.Println("Exiting application")
 }
