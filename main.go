fc0b1ca8adcf8e0ed9ea5eef3e8ed76c4e9da57e
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2021-05-08 11:21:48 -0700

    Update main.go
    Changed the functionality so that -inputdata flag was added so that users can pass in data from the the command line. Also updated -generate so that next string is passed in as a string instead of a list of integer values.

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/sflewis2970/factorsandprimes/algebra"
+	commonutils "github.com/sflewis2970/factorsandprimes/common"
 	"github.com/sflewis2970/factorsandprimes/mathutils"
 )
 
@@ -16,8 +16,9 @@ const (
 	FACTOR = "-FACTOR"
 	// PRIME ...Determine if numbers are prime numbers
 	PRIME = "-PRIME"
+	// INPUTDATA ...Use the following values as input
+	INPUTDATA = "-INPUTDATA"
 	// GENERATE ...Generate {N} numbers
-	// MAXVALUE ...Max value of generated number
 	GENERATE = "-GENERATE"
 )
 
@@ -26,6 +27,7 @@ type applOptions struct {
 	prime          bool
 	nbrsToGenerate int
 	maxValue       int
+	nbrsList       []int
 }
 
 func parseArgs(applArgsList []string) applOptions {
@@ -41,50 +43,53 @@ func parseArgs(applArgsList []string) applOptions {
 			applOptionsData.factor = true
 		case PRIME:
 			applOptionsData.prime = true
-		case GENERATE:
-			// Calculate number of parameters remaining
-			nbrsOfParamsRemaining := (applArgsListSize - 1) - idx
+		case INPUTDATA:
+			// Advance to the next string
+			idx++
 
-			// Parse the remainging parameters
-			if nbrsOfParamsRemaining > 0 {
-				// advance the loop pointer
-				idx++
+			// Make sure that the idx is still in range
+			if idx < applArgsListSize {
+				// make sure the next string is NOT the next parameter option
+				nextStr := applArgsList[idx]
 
-				if idx < applArgsListSize {
-					nbrsToGenerate, err := strconv.Atoi(applArgsList[idx])
+				if strings.Contains(nextStr, "-") {
+					log.Fatal("parameter missing - data values expected\n")
+				} else {
+					nbrListStr := commonutils.SplitTxt(nextStr, " ")
+					nbrsList, err := commonutils.BuildNbrList(nbrListStr)
 					if err != nil {
-						errorMsg := fmt.Sprintf("Error: %v\n", err)
-						log.Fatal(errorMsg)
+						log.Fatal(err)
 					}
 
-					applOptionsData.nbrsToGenerate = nbrsToGenerate
-				} else {
-					log.Fatal("Error parsing nbrsToGenerate")
-				}
-
-				nbrsOfParamsRemaining--
-				if nbrsOfParamsRemaining > 0 {
-					// advance the loop pointer
-					idx++
-
-					if idx < applArgsListSize {
-						maxValue, err := strconv.Atoi(applArgsList[idx])
-						if err != nil {
-							errorMsg := fmt.Sprintf("Error: %v\n", err)
-							log.Fatal(errorMsg)
-						}
-
-						applOptionsData.maxValue = maxValue
-					} else {
-						log.Fatal("Error parsing maxValue")
-					}
-				} else {
-					log.Fatal("Error maxValue not found")
+					applOptionsData.nbrsList = nbrsList
 				}
 			} else {
-				log.Fatal("Error: missing generate parameter values")
+				log.Fatal("parameter missing - data values expected\n")
 			}
+		case GENERATE:
+			// Advance to the next string
+			idx++
 
+			// Make sure that the idx is still in range
+			if idx < applArgsListSize {
+				// make sure the next string is NOT the next parameter option
+				nextStr := applArgsList[idx]
+
+				if strings.Contains(nextStr, "-") {
+					log.Fatal("parameter missing - data values expected\n")
+				} else {
+					nbrListStr := commonutils.SplitTxt(nextStr, " ")
+					nbrsList, err := commonutils.BuildNbrList(nbrListStr)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					applOptionsData.nbrsToGenerate = nbrsList[0]
+					applOptionsData.maxValue = nbrsList[1]
+				}
+			} else {
+				log.Fatal("parameter missing - data values expected\n")
+			}
 		default:
 			errorMsg := fmt.Sprintf("Error: Option not supported: %s.\n", applArgsList[idx])
 			log.Fatal(errorMsg)
@@ -94,6 +99,15 @@ func parseArgs(applArgsList []string) applOptions {
 	// Verify that either -factor or -prime flag has been applied
 	if !applOptionsData.factor && !applOptionsData.prime {
 		log.Fatal("Usage error: either -factor or -prime flag is required")
+	}
+
+	nbrsListSize := len(applOptionsData.nbrsList)
+	if nbrsListSize == 0 && applOptionsData.nbrsToGenerate == 0 && applOptionsData.maxValue == 0 {
+		log.Fatal("Usage error: either -inputdata '{data values}' or -generate '{nbrsToGenerate} {maxValue}' is required")
+	}
+
+	if nbrsListSize > 0 && (applOptionsData.nbrsToGenerate > 0 && applOptionsData.maxValue > 0) {
+		log.Fatal("Usage error: only -inputdata '{data values}' or -generate '{nbrsToGenerate} {maxValue}' is required")
 	}
 
 	return applOptionsData
@@ -149,47 +163,32 @@ func main() {
 		// parse args from command-line
 		applOptionsData = parseArgs(applArgs)
 	} else {
-		// Display usage message
-		fmt.Println("Usage Error")
-
 		// Exit the application
-		log.Fatal("Usage: './main [<-factor>] [<-prime>] [<-generate> [{nbrOfGeneratedValues}] [{maxValue}]]'")
+		log.Fatal("Usage: './main <-factor> <-prime> <<-inputdata> '{numbers separated by space}'> <<-generate> '{nbrOfGeneratedValues} {maxValue}'>'")
 	}
 
 	// Generate numbers requested by user input
-	var nbrsList []int
-	if applOptionsData.nbrsToGenerate > 0 {
+	nbrsListSize := len(applOptionsData.nbrsList)
+	if nbrsListSize == 0 {
+		fmt.Printf("nbrs list is empty, generating numbers\n")
 		newList, err := mathutils.GenerateInts(applOptionsData.nbrsToGenerate, applOptionsData.maxValue)
-
 		if err != nil {
 			errorMsg := fmt.Sprintf("Error generating numbers, err - %v\n", err)
 			log.Fatal(errorMsg)
 		}
 
 		// Update the numbers list
-		nbrsList = newList
-	} else {
-		// Check to see if the nbrsList is empty,
-		// If the nbrsList is empty, have the user enter the numbers that will be used
-		newList, err := algebra.Numbers()
-
-		if err != nil {
-			errorMsg := fmt.Sprintf("Error building factors, err - %v\n", err)
-			log.Fatal(errorMsg)
-		}
-
-		// Updatge the numbers list
-		nbrsList = newList
+		applOptionsData.nbrsList = newList
 	}
 
 	// Build factors map
 	if applOptionsData.factor {
-		BuildFactorsMap(nbrsList)
+		BuildFactorsMap(applOptionsData.nbrsList)
 	}
 
 	// Build primes map
 	if applOptionsData.prime {
-		BuildPrimesMap(nbrsList)
+		BuildPrimesMap(applOptionsData.nbrsList)
 	}
 
 	// Ending string
